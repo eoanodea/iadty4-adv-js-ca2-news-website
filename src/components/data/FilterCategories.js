45b6d4b693f9c6e768df5cba49bc571e12a981c7
Eoan O Dea <odeaeoan@gmail.com>
2021-01-19 13:47:03 +0000

    Code cleanup

--- src/components/data/FilterCategories.js
+++ src/components/data/FilterCategories.js
@@ -6,28 +6,19 @@
  * Author: Eoan O'Dea (eoan@web-space.design)
  * -----
  * File Description:
- * Last Modified: Tuesday, 19th January 2021 1:41:07 pm
+ * Last Modified: Tuesday, 19th January 2021 1:46:42 pm
  * Modified By: Eoan O'Dea (eoan@web-space.design>)
  * -----
  * Copyright 2021 WebSpace, WebSpace
  */
 
 import React from "react";
-import { TextField, Checkbox, makeStyles, withStyles } from "@material-ui/core";
+import { TextField, Checkbox } from "@material-ui/core";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import {
   CheckBox as CheckBoxIcon,
   CheckBoxOutlineBlank,
 } from "@material-ui/icons";
-
-const styles = makeStyles(({ spacing }) => ({
-  root: {
-    width: 500,
-    "& > * + *": {
-      marginTop: spacing(3),
-    },
-  },
-}));
 
 const icon = <CheckBoxOutlineBlank fontSize="small" />;
 const checkedIcon = <CheckBoxIcon fontSize="small" />;
@@ -38,44 +29,41 @@ const FilterCategories = ({
   defaultValueIndex = null,
 }) => {
   if (categories && categories.length > 0) {
-    console.log("default valueindex!!", defaultValueIndex);
     return (
-      <div>
-        <Autocomplete
-          multiple
-          id="filter-categories"
-          options={categories}
-          getOptionLabel={(option) => option.title}
-          disableCloseOnSelect
-          limitTags={3}
-          defaultValue={
-            defaultValueIndex ? [categories[defaultValueIndex]] : undefined
-          }
-          onChange={(event, value) => selectCategory(value)}
-          renderOption={(option, { selected }) => (
-            <React.Fragment>
-              <Checkbox
-                icon={icon}
-                checkedIcon={checkedIcon}
-                style={{ marginRight: 8 }}
-                checked={selected}
-              />
-              {option.title}
-            </React.Fragment>
-          )}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              variant="outlined"
-              label="Filter by Category"
-              placeholder="Basketball"
+      <Autocomplete
+        multiple
+        id="filter-categories"
+        options={categories}
+        getOptionLabel={(option) => option.title}
+        disableCloseOnSelect
+        limitTags={3}
+        defaultValue={
+          defaultValueIndex ? [categories[defaultValueIndex]] : undefined
+        }
+        onChange={(event, value) => selectCategory(value)}
+        renderOption={(option, { selected }) => (
+          <React.Fragment>
+            <Checkbox
+              icon={icon}
+              checkedIcon={checkedIcon}
+              style={{ marginRight: 8 }}
+              checked={selected}
             />
-          )}
-        />
-      </div>
+            {option.title}
+          </React.Fragment>
+        )}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="outlined"
+            label="Filter by Category"
+            placeholder="Basketball"
+          />
+        )}
+      />
     );
   }
   return <></>;
 };
 
-export default withStyles(styles)(FilterCategories);
+export default FilterCategories;
