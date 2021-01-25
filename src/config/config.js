c0afe9a97a57da4dc3495de41562448edef606a6
Eoan O Dea <odeaeoan@gmail.com>
2021-01-25 19:01:17 +0000

    gh pages for deployment & updated server URL

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,7 @@
  * Author: Eoan O'Dea (eoan@web-space.design)
  * -----
  * File Description:
- * Last Modified: Tuesday, 5th January 2021 1:50:09 pm
+ * Last Modified: Monday, 25th January 2021 7:01:14 pm
  * Modified By: Eoan O'Dea (eoan@web-space.design>)
  * -----
  * Copyright 2021 WebSpace, WebSpace
@@ -14,5 +14,5 @@
 
 export const config = {
   env: process.env.NODE_ENV || "development",
-  server_url: "http://localhost:8000",
+  server_url: process.env.REACT_APP_SERVER_URL || "http://localhost:8000",
 };
